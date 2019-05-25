160b49c64f2f318cec112b0efbaf5cb357b2dde1
Admul <thenordgames@gmail.com>
2019-05-25 16:42:35 +0500

    Deleting files in a folder with a certain "key" in the name

--- Deleting files/Program.cs
+++ Deleting files/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Deleting_files
 {
@@ -10,6 +11,33 @@ namespace Deleting_files
     {
         static void Main(string[] args)
         {
+            int countf = 0, count = 0;
+
+            Console.WriteLine("Введите путь к папке в которой хранятся удаляемые файлы: ");
+            string path = Console.ReadLine();
+            Console.WriteLine("Введите ключ удаления файлов: ");
+            string name = Console.ReadLine();
+
+            //Получаем пути файлов
+            DirectoryInfo info = new DirectoryInfo(path);
+            DirectoryInfo[] dirs = info.GetDirectories();
+            FileInfo[] files = info.GetFiles();
+
+            //Считаем кол-во нужных файлов в папке
+            for (int i = 0; i < files.Length; i++)
+                if (files[i].Name.Contains(name))
+                    countf++;
+
+            //Удаляем ненужные файлы
+            for (int i = 0; i < files.Length; i++)
+                if (files[i].Name.Contains(name))
+                {
+                    count++;
+                    files[i].Delete();
+                    Console.WriteLine($"Удаление {files[i].Name}\nПрогресс: {count}/{countf}");
+                }
+
+            Console.ReadKey();
         }
     }
 }
